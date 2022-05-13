56840cee246f38f2f1dfb3db639029743c883217
Ian <Ian.Mohn1@gmail.com>
2022-05-13 00:59:58 -0500

    updated generateHTML file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,4 +1,7 @@
 const path = require('path');
 const fs = require('fs');
-
 const templatesDir = path.resolve(_dirname, "../templates");
+
+const generateHTML = employees => {
+    const html = [];
+}
