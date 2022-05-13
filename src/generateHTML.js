8fb93cac5ec25b347bf2ca86f3517f8bf1a0b7c8
Ian <Ian.Mohn1@gmail.com>
2022-05-13 18:36:59 -0500

    Updated generateHTML file to include filter/map arrow functions

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,6 +2,24 @@ const path = require('path');
 const fs = require('fs');
 const templatesDir = path.resolve(_dirname, "../templates");
 
+// generateHTML function for filtering through the various employee roles
 const generateHTML = employees => {
     const html = [];
-}
+
+    html.push(...employees
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+
+    html.push(...employees
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        );
+
+    html.push(...employees
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        );
+
+    return generateMain(html.join(""));
+};
